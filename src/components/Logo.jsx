39fd89a2f39fa69a07c11d1a150af1b7bc2206b6
reillyse <reillyse@gmail.com>
2022-11-23 20:31:57 -0800

    adding in changes to icon

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
-import { ThemeSelector } from './ThemeSelector'
+
 export function Logomark(props) {
   return (
-      <Image height="36" width="36" src="/images/icon-standard-color-rgb.svg" alt="Logo" />
+      <Image  height="1" width="135" className="h-9 w-auto" src="/images/icon-standard-color-rgb.svg" alt="Brisk Icon" />
   )
 }
 
 export function Logo(props) {
 
-      return ( <span> <span className="display:inline dark:hidden"><Image  height="1" width="135" src="/images/logo-standard-color-rgb.svg" alt="Logo" /> </span>
-      <span className="hidden dark:inline"> <Image  height="1" width="135" src="/images/logo-reversed-color-rgb.svg" alt="Logo" /> </span></span> )
+      return ( <span> <span className="display:inline dark:hidden"><Image  height="1" width="135" src="/images/logo-standard-color-rgb.svg" alt="Brisk Logo" /> </span>
+      <span className="hidden dark:inline"> <Image  height="1" width="135" src="/images/logo-reversed-color-rgb.svg" alt="Brisk Logo" /> </span></span> )
     }
 
